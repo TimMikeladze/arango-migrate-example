a074961b54257cfde1e4861f42f98b0d9aef428f
TimMikeladze <tim.mikeladze@gmail.com>
2022-02-04 11:44:18 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# arango-migrate-example
+
+Sample repository for a minimal example of the arango-migrate cli tool.
+
+## How to use
+
+> npm install
+
+> npx arango-migrate --up
+
+> npx arango-migrate --list
---
+++ migrations/1_initial.js
@@ -0,0 +1,19 @@
+const migration = {
+  description: 'The first migration',
+  async collections () {
+    return ['todo']
+  },
+  async up (db, step) {
+    await step(() => db.collection('todo').save({
+      _key: '1',
+      title: 'Buy milk',
+      completed: false
+    }))
+  },
+  async down (db, step) {
+    await step(() => db.collection('todo').remove({
+      _key: '1'
+    }))
+  }
+}
+module.exports = migration
